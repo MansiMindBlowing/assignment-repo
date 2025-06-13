a016cb106cc2c0644c640efe986be535a3214c0f
mansi kuraria <mansi.kuraria@mindbowser.com>
2025-06-13 16:52:50 +0530

    day 2 work

---
+++ week1/react/day02/dynamicprofile/src/components/BioSection.jsx
@@ -0,0 +1,9 @@
+function BioSection(props){
+    return(
+        <div>
+            <h3>The person is {props.description}</h3>
+        </div>
+    )
+}
+
+export default BioSection
---
+++ week1/react/day02/jsxlayout/src/components/SideBar.jsx
@@ -0,0 +1,9 @@
+function SideBar(){
+    return(
+        <div>
+            <h3 style={{position:"fixed", left:0, width:"17%",height:"100%",top:"32px", border:"2px solid red"}}>SideBar</h3>
+        </div>
+    )
+}
+
+export default SideBar;
