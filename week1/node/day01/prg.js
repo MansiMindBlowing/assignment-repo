2208b1a2dd6c3c474cc5b78a7d440bdd377fd296
mansi kuraria <mansi.kuraria@mindbowser.com>
2025-06-16 18:41:33 +0530

    day1 updated

---
+++ week1/node/day01/prg.js
@@ -0,0 +1,9 @@
+console.log("10 + 20 =", 10 + 20);
+console.log("33 + 55 =", 33 + 55);
+
+const os = require('os');
+
+console.log("Platform:", process.platform);
+console.log("Node.js Version:", process.version);
+console.log("OS Type:", os.type());
+console.log("User Info:", os.userInfo());
